fbd7ffbc5a631ed08c92e3a071be1864ece24ccb
PhucDT <dinhthaiphuc463@gmail.com>
2025-04-28 02:23:01 +0700

    fix(APIs): fix api allcode

--- src/route/web.js
+++ src/route/web.js
@@ -29,7 +29,7 @@ let initWebRoutes = (app) => {
     router.put(`/api/edit-user`, userController.handleEditUser);  // edit user
     router.delete(`/api/delete-user`, userController.handleDeleteUser); // delete user
 
-    router.get('/allcode', userController.getAllCode);
+    router.get('/api/allcode', userController.getAllCode);
 
 
     return app.use("/", router);
