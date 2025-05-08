36877082dfc30f5a7a2d9d506e220a3a92d9715b
PhucDT <dinhthaiphuc463@gmail.com>
2025-05-08 23:11:18 +0700

    feat: tao api top-doctor-home, tao moi doctorController, tao moi doctorService, sua lai key thanh keyMap trong migration db allcode , them cac associate vao allcode va user

---
+++ src/services/doctorService.js
@@ -0,0 +1,33 @@
+import db from "../models/index";
+let getTopDoctorHomeService = (limitInput) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = await db.User.findAll({
+                limit: limitInput,
+                where: { roleId: 'R2' }, // R2 la doctor
+                order: [['createdAt', 'DESC']], // xep theo ngay create
+                attributes: {
+                    exclude: ['password'] // hide password, image
+                },
+                include: [
+                    { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    { model: db.Allcode, as: 'roleData', attributes: ['valueEn', 'valueVi'] },
+                    { model: db.Allcode, as: 'genderData', attributes: ['valueEn', 'valueVi'] },
+
+                ],
+                raw: true,
+                nest: true,
+            })
+            resolve({
+                errCode: 0,
+                data: users
+            })
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    getTopDoctorHomeService: getTopDoctorHomeService,
+}
